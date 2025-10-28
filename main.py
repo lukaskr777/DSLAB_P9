443480dbb00d1894b6fff8c2c93bfc8b7d7b946e
lukaskr777 <76201682+lukaskr777@users.noreply.github.com>
2025-10-28 11:13:30 +0100

    setup

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import glob
+
+
+
+
+
+
+if __name__ == "__main__":
+    files = glob.glob("Datasets/litellm/public.LiteLLM_SpendLogs/1/*.parquet")  # adjust path
+    first_file = files[0]                # pick the first file
+    df = pd.read_parquet(first_file)
+
+    print(df.describe())
+
+    df.plot(x="total_tokens", y="total_spend", kind="line")
+
